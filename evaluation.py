7072cfbd757c72deb83c9bcab3f8b61a93a20fe8
Vlad Sobol <vladsobol@nyu.edu>
2020-05-16 18:42:20 -0400

    some refactoring

--- evaluation.py
+++ evaluation.py
@@ -1,17 +1,21 @@
-print('about to import xrv')
-from torchxrayvision import datasets as xrv_datasets
-print('import xrv')
-import numpy as np
-import logging
-
-from sklearn.metrics import roc_auc_score
-
-
 from models import Baseline
+from sklearn.metrics import roc_auc_score
+import logging
+import numpy as np
+from torchxrayvision import datasets as xrv_datasets
+from data import COVID19_Dataset
 
-COVID_19_DATASET_PATH = '~/vlad/machine_learning_chest_datasets/covid-chestxray-dataset'
-COVID_19_DATASET_IMAGES_PATH = '~/vlad/machine_learning_chest_datasets/covid-chestxray-dataset/images'
-COVID_19_DATASET_METADATA_PATH = '~/vlad/machine_learning_chest_datasets/covid-chestxray-dataset/metadata.csv'
+
+def get_test_folds_indices(dataset_length, folds):
+    permutation = np.random.permutation(dataset_length)
+    size = dataset_length // folds
+    remainder = dataset_length % folds
+    sizes = np.array([size] * folds)
+    sizes[:remainder] += 1
+    assert sizes.sum() == dataset_length
+    points = np.cumsum(sizes)[:-1]
+    split = np.split(permutation, points)
+    return split
 
 
 def partitions_generator(dataset, folds):
@@ -28,14 +32,12 @@ def partitions_generator(dataset, folds):
 
 
 def main():
-    logging.info('about to load the dataset')
-    d_covid19 = xrv_datasets.COVID19_Dataset(imgpath=COVID_19_DATASET_IMAGES_PATH,
-                                             csvpath=COVID_19_DATASET_METADATA_PATH)
-    logging.info('loaded the dataset')
+    d_covid19 = COVID19_Dataset()
     logging.info(f'entire dataset length is {len(d_covid19)}')
 
     for i, (train_dataset, test_dataset) in enumerate(partitions_generator(d_covid19, 10)):
-        logging.info(f'train size {len(train_dataset)}, test size {len(test_dataset)}')
+        logging.info(
+            f'train size {len(train_dataset)}, test size {len(test_dataset)}')
         model = Baseline()
         model.fit(train_dataset)
         performance = np.zeros(len(test_dataset.pathologies))
@@ -50,23 +52,11 @@ def main():
         ground_truth = np.stack(ground_truth)
 
         for i in range(len(test_dataset.pathologies)):
-            if ground_truth[:, i].sum() > 0:
-                performance[i] = roc_auc_score(ground_truth[:, i], predictions[:, i])
+            if np.unique(ground_truth[:, i]).shape[0] > 1:
+                performance[i] = roc_auc_score(
+                    ground_truth[:, i], predictions[:, i])
         performance /= len(test_dataset)
-        logging.info(f'At fold {i} accuracy is:\n{performance}')
-
-
-
-def get_test_folds_indices(dataset_length, folds):
-    permutation = np.random.permutation(dataset_length)
-    size = dataset_length // folds
-    remainder = dataset_length % folds
-    sizes = np.array([size] * folds)
-    sizes[:remainder] += 1
-    assert sizes.sum() == dataset_length
-    points = np.cumsum(sizes)[:-1]
-    split = np.split(permutation, points)
-    return split
+        logging.info(f'At fold {i} per class AUC is:\n{performance}')
 
 
 if __name__ == '__main__':
