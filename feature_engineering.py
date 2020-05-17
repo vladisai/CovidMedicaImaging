2a8225a760dbd1977faf37248af494c65a9c5744
Artem Vysogorets <artem@vysogorets.org>
2020-05-17 22:57:08 +0300

    added fourier transform

--- feature_engineering.py
+++ feature_engineering.py
@@ -18,12 +18,14 @@ def load_data(input_path,target_size=None,crop_by_pixels=None,crop_by_factors=No
   try:
     img_names=[img_name for img_name in listdir(input_path) if (isfile(join(input_path,img_name)) and not img_name.startswith('.'))]
     logging.info(f'Number of files read: {len(img_names)}')
+    if len(img_names)<1:
+      raise ValueError("no images read")
   except:
     raise ValueError("input_path dir not found / could not read")
   images=[cv2.imread(join(input_path,img_name)) for img_name in img_names]
   try:
     images=list(map(lambda x: cv2.cvtColor(x,cv2.COLOR_BGR2GRAY),images))
-    logging.info('Images coverted to greyscale')
+    logging.info('Images converted to greyscale')
   except:
     logging.info('Images are greyscale')
   target_size=target_size or images[0].shape
@@ -47,7 +49,6 @@ def load_data(input_path,target_size=None,crop_by_pixels=None,crop_by_factors=No
 def get_lbp(images,P=8,R=1,**kwargs):
   lbp_images=list(map(lambda x: F.local_binary_pattern(x,R=R,P=P),images))
   lbp_hists=list(map(lambda x: np.histogram(x.reshape(-1),bins=256,range=(0,256))[0],lbp_images))
-  print(len(lbp_images))
   return lbp_hists,lbp_images
 
 def get_hog(images,visualize=True,dim_reduction_factor=1.0,bins=9,cells_per_block=(2,2),**kwargs):
@@ -68,15 +69,27 @@ def get_hog(images,visualize=True,dim_reduction_factor=1.0,bins=9,cells_per_bloc
   else:
     return hog_features
 
-def main(input_path,output_path,lbp,hog,lbp_kwargs={},hog_kwargs={},preprocessing_kwargs={}):
+def get_fourier(images):
+  dfts=[np.fft.fft2(x) for x in images]
+  mags=np.asarray([20*np.log(np.abs(x)) for x in dfts],dtype=np.uint8)
+  shifted=[np.fft.fftshift(x) for x in mags]
+  return [np.divide(ex,np.max(ex)) for ex in shifted],shifted
+
+def main(input_path,output_path,fft,lbp,hog,lbp_kwargs={},hog_kwargs={},preprocessing_kwargs={}):
   logging.getLogger().setLevel(logging.INFO)
   data=load_data(input_path,**preprocessing_kwargs)
   features={}
   images={}
+  if fft:
+    fft_features,fft_images=get_fourier(data)
+    images['fft']=fft_images
+    features['fft']=fft_features
+    logging.info("Computed fft")
   if lbp:
     lbp_features,lbp_images=get_lbp(data,**lbp_kwargs)
     features['lbp']=lbp_features
     images['lbp']=lbp_images
+    logging.info("Computed lbp")
   if hog:
     hog_features=get_hog(data,**hog_kwargs)
     if len(hog_features)>1:
@@ -84,6 +97,7 @@ def main(input_path,output_path,lbp,hog,lbp_kwargs={},hog_kwargs={},preprocessin
       hog_features=hog_features[0]
       images['hog']=hog_images
     features['hog']=hog_features
+    logging.info("Computed hog")
   data=[features,images]
   save_data(output_path,data)
   logging.info(f"Files written to '{output_path}'")
@@ -92,6 +106,7 @@ if __name__=='__main__':
   parser=argparse.ArgumentParser()
   parser.add_argument('--input_path',type=str,default=None)
   parser.add_argument('--output_path',type=str,default=None)
+  parser.add_argument('--fft',type=int,default=1) # flag to compute fourier transform
   parser.add_argument('--lbp',type=int,default=1) # flag to compute local binary patterns
   parser.add_argument('--hog',type=int,default=1) # flag to compute histogram of gradients
   parser.add_argument('--lbp_kwargs',type=str,default='{}') # supports 'R':int (radius) and 'P':int (number of points)
@@ -102,4 +117,4 @@ if __name__=='__main__':
   # 'crop_by_pixels':tuple(int,int) (crop the central piece of given height and width out of all images), 'crop_by_factors':tuple(float,float):
   # crops the central part of each image leaving only the given fractions of the original dimensions. note that cropping (if any) is applied after resizing (if any).
   args=parser.parse_args()
-  main(args.input_path,args.output_path,args.lbp,args.hog,eval(args.lbp_kwargs),eval(args.hog_kwargs),eval(args.preprocessing_kwargs))
+  main(args.input_path,args.output_path,args.fft,args.lbp,args.hog,eval(args.lbp_kwargs),eval(args.hog_kwargs),eval(args.preprocessing_kwargs))
