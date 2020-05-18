809ac690aa9a4ec3bc0a65b2d351c3859a71f0c3
Artem Vysogorets <artem@vysogorets.org>
2020-05-19 00:30:04 +0300

    now accepts 'feature_num' argument

--- param.py
+++ param.py
@@ -7,6 +7,7 @@ import torch
 def parse_args():
         parser = argparse.ArgumentParser()
         parser.add_argument('--seed', type=int, default=20, help='random seed')
+        parser.add_argument('--feature_num', type=int, default=10000, help='number of features with FeatureExtractor')
         parser.add_argument('--classifier', type=str, default='LogisticRegression')
         parser.add_argument('--PCA', action='store_const', default=False, const=True)
         parser.add_argument('--pca_out_dim', type=int, default=1000, help='Number of dimensions to keep after PCA')
